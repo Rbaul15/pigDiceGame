b0066f76af64682eb0fccbefcbd2bd09fb80a079
Ryan Baul <rbaul15@gmail.com>
2018-07-07 14:04:41 -0700

    updated file structure

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		//  2. Display the result
 		var diceDOM = document.querySelector('.dice');
 		diceDOM.style.display = 'block';
-		diceDOM.src = 'dice-' + dice + '.png';
+		diceDOM.src = 'img/dice-' + dice + '.png';
 
 
 		//  3. Update the round score IF the rolled number was not a 1
