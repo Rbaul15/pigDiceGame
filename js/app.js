6de8723bb3f87eda81e966c261ca5a63606f1604
Ryan Baul <rbaul15@gmail.com>
2018-07-08 20:38:19 -0700

    added second dice to game

--- js/app.js
+++ js/app.js
@@ -6,8 +6,10 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 75 points on GLOBAL score wins the game
-
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
+
 
 var scores, roundScore, activePlayer, gamePlaying;
 
@@ -17,22 +19,20 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
 		//  1. Random Number
-		var dice = Math.floor(Math.random() * 6) + 1;
-
-		var diceRoll = dice;
-		console.log(diceRoll);
-		console.log(dice);
+		var dice1 = Math.floor(Math.random() * 6) + 1;
+		var dice2 = Math.floor(Math.random() * 6) + 1;
 
 		//  2. Display the result
-		var diceDOM = document.querySelector('.dice');
-		diceDOM.style.display = 'block';
-		diceDOM.src = 'img/dice-' + dice + '.png';
+		document.getElementById('dice-1').style.display = 'block';
+		document.getElementById('dice-2').style.display = 'block';
+		document.getElementById('dice-1').src = 'img/dice-' + dice1 + '.png';
+		document.getElementById('dice-2').src = 'img/dice-' + dice2 + '.png';
 
 
 		//  3. Update the round score IF the rolled number was not a 1
-		if (dice !== 1) {
+		if (dice1 !== 1 && dice2 !== 1) {
 			// Add score
-			roundScore += dice;
+			roundScore += dice1 + dice2;
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
 		} else {
 			// Next player
@@ -49,10 +49,23 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		// Update the UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+		// Add input value for winning score
+		var input = document.querySelector('.final-score').value;
+		var winningScore;
+
+		// Check if input value is empty...Undefined, 0, null, or "" are COERCED to false
+		// Anything else is coerced to true
+		if (input) {
+			winningScore = input
+		} else {
+			winningScore = 75;
+		}
+
 		// Check if player won the game
-		if (scores[activePlayer] >= 75) {
+		if (scores[activePlayer] >= winningScore) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('#dice-1').style.display = 'none';
+			document.querySelector('#dice-2').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 			gamePlaying = false;
@@ -86,7 +99,8 @@ function init() {
 	gamePlaying = true;
 
 	// Hide dice image with css
-	document.querySelector('.dice').style.display = 'none';
+	document.querySelector('#dice-1').style.display = 'none';
+	document.querySelector('#dice-2').style.display = 'none';
 	// Change HTML score values
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
